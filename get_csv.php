0acbb4b118f0dcb6ff7428540ccc182d1f1ec0f7
yishe <y-choden@ver2.jp>
2016-10-19 18:12:28 +0900

    Compatible with 3.1

--- get_csv.php
+++ get_csv.php
@@ -42,54 +42,63 @@ foreach ($allcourses as $course) {
     $lastc = 0;
 
     if (count($data) > 0) {
-      $myxls->write($lastr, $lastc, get_string('courseid', 'block_attendance_csv_all'), $formatbc); $lastc++;
-      $myxls->write($lastr, $lastc, get_string('coursename', 'block_attendance_csv_all'), $formatbc); $lastc++;
-      $myxls->write($lastr, $lastc, get_string('firstname', 'block_attendance_csv_all'), $formatbc); $lastc++;
+        $myxls->write($lastr, $lastc, get_string('courseid', 'block_attendance_csv_all'), $formatbc); $lastc++;
+        $myxls->write($lastr, $lastc, get_string('coursename', 'block_attendance_csv_all'), $formatbc); $lastc++;
+        $myxls->write($lastr, $lastc, get_string('firstname', 'block_attendance_csv_all'), $formatbc); $lastc++;
 
-      $statused = array();
+        $statused = array();
 
-      foreach ($data as $att) {
-        if ($insts = $DB->get_records_sql("SELECT * FROM {attendance_sessions} WHERE attendanceid = ? AND sessdate >= ? AND sessdate < ? ORDER BY sessdate ASC", array($att->id, $timefrom, $timeto))) {
-          $ci = 0;
-          foreach($insts as $instance) {
-            $ci++;
-            if ($ci < 10)
-              $name = get_string('class', 'block_attendance_csv_all')."0".$ci;
-            else
-              $name = get_string('class', 'block_attendance_csv_all').$ci;
+        foreach ($data as $att) {
+            if ($insts = $DB->get_records_sql("SELECT * FROM {attendance_sessions} WHERE attendanceid = ? AND sessdate >= ? AND sessdate < ? ORDER BY sessdate ASC", array($att->id, $timefrom, $timeto))) {
+                $ci = 0;
+                foreach($insts as $instance) {
+                    $ci++;
+                    if ($ci < 10) {
+                        $name = get_string('class', 'block_attendance_csv_all') . "0" . $ci;
+                    } else {
+                        $name = get_string('class', 'block_attendance_csv_all') . $ci;
+                    }
 
-            $myxls->write($lastr, $lastc, $name, $formatbc); $lastc++;
+                    $myxls->write($lastr, $lastc, $name, $formatbc); $lastc++;
 
-            if (empty($statused[$instance->attendanceid])) {
-              $s = $DB->get_record_sql("SELECT id FROM {attendance_statuses} WHERE attendanceid = ? AND description LIKE '%Absent%' LIMIT 1", array($instance->attendanceid));
-              $statused[$instance->attendanceid] = $s ? $s->id : null;
+                    if (empty($statused[$instance->id])) {
+                        $sql = "
+                            SELECT s.id
+                            FROM {attendance_statuses} s
+                            INNER JOIN (
+                                SELECT MIN(grade) as mingrade, attendanceid, setnumber
+                                FROM {attendance_statuses}
+                                WHERE attendanceid = ? AND setnumber = ?
+                            ) st ON s.attendanceid = st.attendanceid AND s.grade = st.mingrade AND s.setnumber = st.setnumber
+                        ";
+
+                        $s = $DB->get_record_sql($sql, array($instance->attendanceid, $instance->statusset));
+                        $statused[$instance->id] = $s ? $s->id : null;
+                    }
+                }
+
+                foreach($students as $student) {
+                    $lastr++;
+                    $lastc = 0;
+
+                    $myxls->write($lastr, $lastc, $course->idnumber); $lastc++;
+                    $myxls->write($lastr, $lastc, $course->shortname); $lastc++;
+                    $myxls->write($lastr, $lastc, $student->firstname); $lastc++;
+
+                    foreach($insts as $instance) {
+                        if ($att = $DB->get_record('attendance_log', array('sessionid' => $instance->id, 'studentid' => $student->id, 'statusid' => $statused[$instance->id]))) {
+                            $myxls->write($lastr, $lastc, strftime("%Y/%m/%d", $instance->sessdate)); $lastc++;
+                        } else {
+                            $myxls->write($lastr, $lastc, ""); $lastc++;
+                        }
+                    }
+                }
+
+                $lastr++;
             }
-          }
-
-
-
-          foreach($students as $student) {
-            $lastr++;
-            $lastc = 0;
-
-            $myxls->write($lastr, $lastc, $course->idnumber); $lastc++;
-            $myxls->write($lastr, $lastc, $course->shortname); $lastc++;
-            $myxls->write($lastr, $lastc, $student->firstname); $lastc++;
-
-            foreach($insts as $instance) {
-              if ($att = $DB->get_record('attendance_log', array('sessionid' => $instance->id, 'studentid' => $student->id, 'statusid' => $statused[$instance->attendanceid]))) {
-                $myxls->write($lastr, $lastc, strftime("%Y/%m/%d", $instance->sessdate)); $lastc++;
-              }else {
-                $myxls->write($lastr, $lastc, ""); $lastc++;
-              }
-            }
-          }
-
-          $lastr++;
         }
-      }
 
-      $lastr++;
+        $lastr++;
     }
 }
 
